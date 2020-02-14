f49608938c6fdbefc7d6ee8570916f63515833bf
Ezequiel Ledesma <ezequiel.ledesma026@gmail.com>
2020-02-14 19:01:55 -0300

    subo cambios

--- test/funcional/ModificarAnecdotaTest.php
+++ test/funcional/ModificarAnecdotaTest.php
@@ -37,7 +37,7 @@ class ModificarAnecdota extends TestCase
         
         $this->webDriver->get('http://turismonacionaleinternacional/modelo/agenda.php');
 
-        $this->webDriver->findElement(WebDriver\WebDriverBy::xpath('//button[@type="button"]'))->click();
+        $this->webDriver->findElement(WebDriver\WebDriverBy::xpath("//button[@type='button']"))->click();
    
         $this->webDriver->findElement(WebDriver\WebDriverBy::id('usuario'))->click();
 
